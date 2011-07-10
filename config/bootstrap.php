15df1533ed1eaf9f75837a8e070d47bb63b79871
Nate Abele <nate.abele@gmail.com>
2011-07-10 10:59:45 -0700

    Updating copyright and removing leading backslash on namespace name.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,12 +2,11 @@
 /**
  * Lithium: the most rad php framework
  *
- * @copyright     Copyright 2010, Union of RAD (http://union-of-rad.org)
+ * @copyright     Copyright 2011, Union of RAD (http://union-of-rad.org)
  * @license       http://opensource.org/licenses/bsd-license.php The BSD License
  */
 
 use lithium\net\http\Media;
-
 
 /**
  * Register the design view as the default handler.
@@ -15,7 +14,7 @@ use lithium\net\http\Media;
  * @see li3_design\extensions\View
  */
 Media::type('default', null, array(
-	'view' => '\li3_design\extensions\View',
+	'view' => 'li3_design\extensions\View',
 	'paths'    => array(
 		'template' => '{:library}/views/{:controller}/{:template}.{:type}.php',
 		'layout'   => '{:library}/views/layouts/{:layout}.{:type}.php',
